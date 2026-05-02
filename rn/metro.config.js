5d27578d0d3a7e259af394db2b78b701d07f3cde
AppSauce <trae-assistant@exasstant>
2026-05-02 02:10:18 -0600

    Stabilize Android verification and canonicalize Expo app surface

--- rn/metro.config.js
+++
@@ -1,7 +0,0 @@
-const { getDefaultConfig } = require('expo-router/metro')
-const { withNativeWind } = require('nativewind/metro')
-
-const config = getDefaultConfig(__dirname)
-
-module.exports = withNativeWind(config, { input: './global.css' })
-
